3e79baffad840378c04575722a1a53115c4a6c36
memorstx <memorstx@gmail.com>
2023-12-19 10:59:00 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+// Escribe una función llamada isEvenOrOdd que tome un número como parámetro y devuelva true si es par y false si es impar.
+
+// export function isEvenOrOdd(number) {
+//     return number % 2 === 0; 
+// }
+// const numberToCheck = 0;
+// const result = isEvenOrOdd(numberToCheck);
+// result ? console.log(`El número ${numberToCheck} es par`) : console.log(`El número ${numberToCheck} es impar`);
+
+
+
+// -----------------------------
+// Escribe una función llamada isMultipleOfFive que tome un número como parámetro y devuelva true si el número es múltiplo de 5 y false en caso contrario. 
+// const numberToCheck = 19;
+// export function isMultipleOfFive(number){
+//     return number % 5 === 0
+// }
+
+// const result = isMultipleOfFive(numberToCheck);
+
+// result ? console.log(`El número ${numberToCheck} sí es múltiplo de 5`) : console.log(`El número ${numberToCheck} no es múltiplo de 5`);}
+
+
+
+
+// -------------------------------
+// Escribe una función llamada getGreaterNumber que tome dos números como parámetros y devuelva el número mayor.
+
+// const edadDeJuan = 32;
+// const edadDeAna = 9;
+
+// export function getGreaterNumber(numberA, numberB){
+//     return numberA > numberB ? numberA: numberB;
+// }
+
+// console.log(`La edad mayor es de: ${getGreaterNumber(edadDeJuan, edadDeAna)}`);
+
+
+// Escribe una función llamada getGrade que tome una calificación numérica (un número entre 0 y 100) como parámetro y devuelva la calificación correspondiente. Utiliza la siguiente escala:
+
+// 90-100: A
+// 80-89: B
+// 70-79: C
+// 60-69: D
+// 0-59: F
+
+
